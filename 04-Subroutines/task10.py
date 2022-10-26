9d9fec53c4fb1c29bae454e1b7b80d88c295d656
ValeriiaLisovska <49917083+lerahjat@users.noreply.github.com>
2022-10-26 11:07:27 +0200

    Add files via upload

---
+++ 04-Subroutines/task10.py
@@ -0,0 +1,6 @@
+def read_number():
+   x = int(input("Enter your number:"))
+   return x
+x = read_number()
+y = read_number()
+print(f"Sum of numbers {x+y}")
---
+++ 04-Subroutines/task15.py
@@ -0,0 +1,10 @@
+import random
+import mymath
+def read_number():
+   x = int(input("Enter your number:"))
+   return x
+x = read_number()
+
+def generate_number():
+   random.randint(1,9)
+print(generate_number)
