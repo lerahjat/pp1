aacc6d6fa6b98d3a82f12c761d8349f682f8ca31
lerahjat <lerafox2590@gmail.com>
2022-11-08 18:16:05 +0100

    hw

--- 03-ControlStructures/task25.py
+++ 03-ControlStructures/task25.py
@@ -1,13 +1,8 @@
-hight = int(input())
-wight = int(input())
+height = int(input())
+width = int(input())
 
-for i in range(hight):
-    if i == 0 or i == hight - 1:
-        for j in range(wight):
-            print("*", end='')
+for i in range(height):
+    if i !=0 and i != height-1:
+        print('*' + '  ' * (width-2)+ ' *')
     else:
-        print("*", end='')
-        for j in range(1, wight - 1):
-            print(' ', end='')
-        print("*", end='')
-    print()
+        print("* "*width)
