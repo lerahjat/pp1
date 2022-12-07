ff4dd322ca21f67fcab22eeafe84b6e4bda5ae13
lerahjat <lerafox2590@gmail.com>
2022-12-07 01:08:49 +0100

    hw

---
+++ 09-Test2/test3.py
@@ -0,0 +1,32 @@
+with open("TP.txt", "r") as f:
+    whole_text = f.read()
+
+out = []
+
+def word_finder(f, ll):
+    w_by_w = whole_text.split()
+
+
+    for i in w_by_w:
+
+
+        if i.startswith(f) and i.endswith(ll):
+
+            out.append(i)
+    return out
+
+print(word_finder('m', 'g'))
+
+
+
+
+
+
+
+
+
+# w_by_w = whole_text.split()
+# for i in w_by_w:
+#     if i.startswith('m') and i.endswith('g'):
+#         print(i)
+
